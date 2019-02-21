f4f6cbb6b4fbd612e1b075c2b1aa54f3c0f1cf4b
Mateusz <panczakmateusz@gmail.com>
2019-02-21 19:32:51 +0100

    Dokonczenie zadania

--- src/pl/panczak/mateusz/firstapp/BodyMassIndex.java
+++ src/pl/panczak/mateusz/firstapp/BodyMassIndex.java
@@ -1,42 +1,31 @@
 
-package pl.panczak.mateusz.firstapp;
+package pl.panczak.mateusz.firstapp; //pakiet - lokalizacja klasy w drzewie projektu
 
+public class BodyMassIndex {    //definicja publicznej klasy o nazwie BodyMassIndex
 
-public class BodyMassIndex {
+    private double weight, height; //deklaracja prywatnych pol (hermetyzacja danych) typu double (64b - liczba zmiennoprzecinkowa podwojnej precyzji), mogacych przechowywac liczby niecalkowite takie jak wzrost czy waga czlowieka
 
-double weight;
-double height;
-double bmi;
-
-
-
-    public BodyMassIndex(double weight, double height) {
-        this.weight = weight;
-        this.height = height;
-
-    }
-    public void calculate(){
-        double bmi = weight/(height*height);
-        System.out.format("Twoje BMI to: %f", bmi);
+    public BodyMassIndex(double weight, double height) { //utworzenie konstruktora dwuparametrowego, ktory podczas tworzenia obiektu ustawia wartosci pol odpowiadajacych za wzrost i wage i nie pozwala zostawic ich pustych
+        this.weight = weight; //ustawienie wartosci pola odpowiadajacego za wage - slowo kluczowe "this" wskazuje na pole klasy, zas zmienna "weight" po prawej stronie znaku rownosci to parametr przekazany do konstruktora podczas tworzenia obiektu
+        this.height = height; //ustawienie wartosci pola odpowiadajacego za wzrost - slowo kluczowe "this" wskazuje na pole klasy, zas zmienna "height" po prawej stronie znaku rownosci to parametr przekazany do konstruktora podczas tworzenia obiektu
     }
 
-    public double getBmi() {
-        return bmi;
+    private double calculate() { //deklaracja metody obliczajacej BMI, zwraca ona wartosc typu double (BMI moze byc i czesto nie jest liczba calkowita), metoda nie przyjmuje zadnych parametrow, dziala na polach klasy, metoda jest prywatna gdyz jest jedynie metoda pomocnicza i nie powinna byc widziana z zewnatrz klasy
+
+        return weight / (height * height); //zwrocenie niniejszej wartosci oznacza de facto obliczenie wskaznika BMI
     }
 
-    public void getClassification() {
-
-        //double bmi = weight/(height*height); //o poprawy to
-
-        if (bmi > 30) {
-            System.out.println("Masz nadwagę");
-        } else if (bmi < 18.5) {
-            System.out.println("Masz niedowagę");
-        } else {
-            System.out.println("Masz prawidłową wagę");
+    public String getClassification() { //deklaracja metody zwracajacej wartosc wskaznika BMI oraz wyswietlajacej stosowny komunikat o "stanie" osoby badanej, publiczna - bedzie wywolana w celu wyswietlenia wynikow, nie przyjmuje parametrow, jest typu "String" - zwraca ciag znakow
+        String result; //lokalna zmienna pomocnicza, ktora bedzie przechowywac rezultat dzialania metody i zwracac go na zewnatrz
+        double bmi = calculate(); //lokalna zmienna pomocnicza, ktora bedzie przechowywac wynik dzialania metody "calculate" aby nie musiec obliczac wartosci tejze metody kilkukrotnie w dalszych dzialaniach
+        if (bmi > 30) { //sprawdzenie czy wartosc wskaznika BMI jest wieksze od 30, co przyjeto za dolna granice nadwagi
+            result = "Twoje BMI to: " + bmi + "\nMasz nadwagę"; //przypisanie do zmiennej "result" ciagu znakow stanowiacego konunikat wyswietlany uzytkownikowi, gdy warunek logiczny przy "if" jest prawdziwy, znak plusa oznacza konkatenacje, czyli laczenie znakow, zas "\n", wyswietlenie wszystkich znakow za nim w nowej linii
+        } else if (bmi < 18.5) { //sprawdzenie czy wartosc wskaznika BMI jest mniejsze od 18.5, co przyjeto za gorna granice niedowagi
+            result = "Twoje BMI to: " + bmi + "\nMasz niedowagę"; //przypisanie do zmiennej "result" ciagu znakow stanowiacego konunikat wyswietlany uzytkownikowi, gdy warunek logiczny przy "if" jest prawdziwy, znak plusa oznacza konkatenacje, czyli laczenie znakow, zas "\n", wyswietlenie wszystkich znakow za nim w nowej linii
+        } else { //juz bez warunku, jesli BMI jest wieksze od 18,5 i jednoczesnie mneijsze od 30 to waga jest w porzadku i nie trzeba niczego sprawdzac, tylko taka ewentualnosc pozostaje
+            result = "Twoje BMI to: " + bmi + "\nMasz prawidłową wagę"; //przypisanie do zmiennej "result" ciagu znakow stanowiacego konunikat wyswietlany uzytkownikowi, gdy warunek logiczny przy "if" jest prawdziwy, znak plusa oznacza konkatenacje, czyli laczenie znakow, zas "\n", wyswietlenie wszystkich znakow za nim w nowej linii
         }
-
-
+        return result;  //metoda zwraca ciag znakow przypisany do zmiennej result w zaleznosci od tego, ktory warunek logiczny zostal spelniony
     }
 
 }
--- src/pl/panczak/mateusz/firstapp/Main.java
+++ src/pl/panczak/mateusz/firstapp/Main.java
@@ -1,16 +1,12 @@
-package pl.panczak.mateusz.firstapp;
+package pl.panczak.mateusz.firstapp; //pakiet - lokalizacja klasy w drzewie projektu
 
-import java.util.Scanner;
-public class Main {
+import java.util.Scanner; //zaimportowanie biblioteki scanner (do pobierania tekstu z klaawiatury)
+public class Main { //definicja publicznej klasy Main
 
-    public static void main(String[] args) throws InterruptedException {
-        // write your code here
-
-        // PrintStream ps = new PrintStream(System.out);
-
-//1.3
-        /*
-        System.out.println("Witaj Świecie!\n" +
+    public static void main(String[] args) throws InterruptedException { //metoda main, od niej rozpoczyna sie dzialanie programu w Javie, powinna być publiczna, musi byc statyczna, throws InterruptedException do wykorzystania przy sleep
+//1.3 zakomentowana pierwsza część zadania, aby przetestować należy odkomentować i zakomentować resztę
+//*
+        System.out.println("Witaj Świecie!\n" +    // strumień wyjściowy, wyświetla tekst w konsoli, \n-nowa linia \t-tabulacja
                 "Inwokacja:\n\t" +
                 "\"Litwo! Ojczyzno moja! Ty jesteś jak zdrowie,\n\t" +
                 "Ile cię trzeba cenić, ten tylko się dowie,\n\t" +
@@ -18,19 +14,17 @@ public class Main {
                 "Widzę i opisuję, bo tęsknię po tobie.\"\n" +
                 "Koniec inwokacji. Koniec programu za 10..");
 
-
-
-        int i;
-        for (i=9; i>0; i--){
-            Thread.sleep(1000);
-            System.out.println("Koniec programu za " +i  +"..");
+        int i; //inicjalizaacja zmiennej (intiger) wykorzystanej w petli for
+        for (i=9; i>0; i--){  //pętla for, warunek prawdziwy dopuki i>0
+            Thread.sleep(1000); // uśpienie na 1s zanim zostanie wyświetlona kolejna linia
+            System.out.println("Koniec programu za " +i  +".."); // Wyświetlanie tekstu w konsoli "+" można dokładać kolejne elementy
 
         }
-        */
+// */
 
-//1.4
+//1.4 zakomentowana kolejna część zadania
 /*
-        System.out.println("Inwokacja:\n");
+        System.out.println("Inwokacja:\n"); // strumień wyjściowy, wyświetla tekst w konsoli, \n-nowa linia \t-tabulacja
         System.out.println("\t\"Litwo! Ojczyzno moja! Ty jesteś jak zdrowie,\n" +
                 "\tIle cię trzeba cenić, ten tylko się dowie,\n" +
                 "\tKto cię stracił. Dziś piękność twą w całej ozdobie\n" +
@@ -38,33 +32,72 @@ public class Main {
 
         System.out.println("\n");
         System.out.println("Koniec inwokacji.");
-        System.out.println("Podaj ilość wiadomości:");
-        var sc = new Scanner(System.in); //Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Podaj treść wiadomości:");
-        String message = sc.nextLine();
-        for (int i = x; i > 0; i--) {
+        System.out.println("Podaj ilość wiadomości:"); //wyswietlenie uzytkownikowi komunikatu jaka wartosc ma wprowadzic z klawiatury
+        var sc = new Scanner(System.in); ////utworzenie obiektu typu Scanner, Scanner sc = new Scanner(System.in); zgodnie z instrukcją zamiana Scanner na var
+        int x = sc.nextInt(); // pobranie zmiennej x z klawiatury
+        sc.nextLine(); //jeżeli używa się nextInt nextDouble itp. to po nich należy wywołać pustę metodę nextLine(), ponieważ w buforze pozostaje znak nowej linii \n
+        System.out.println("Podaj treść wiadomości:"); ////wyswietlenie uzytkownikowi komunikatu jaka wartosc ma wprowadzic z klawiatury
+        String message = sc.nextLine(); //pobranie kolejnej zmiennej, tym razem string
+        for (int i = x; i > 0; i--) { //pętla taka jak wcześniej z tym że warunak początkowy x=i -liczbie wprowadzonej z klawiatury
 
-            System.out.format("\n %s  %d...", message, i);
+            System.out.format("\n %s  %d...", message, i); //wypisywanie na ekranie, format- po "%" wpisuje się rodzaj zmiennej a na końcu jej nazwę w odpowiedniej kolejności
         }
 */
-//1.5
+//1.5 kolejna część
 /*
-        var sc = new Scanner(System.in); //Scanner sc = new Scanner(System.in);
+        var sc = new Scanner(System.in);//utworzenie obiektu typu Scanner
 
-        System.out.println("Podaj wagę [kg]:");
-        double weight = sc.nextDouble();
+        System.out.println("Podaj wagę [kg]:");//wyswietlenie uzytkownikowi komunikatu jaka wartosc ma wprowadzic z klawiatury
+        double weight = sc.nextDouble(); //pobranie zmiennej z klawiatury
 
-        System.out.println("Podaj wzrost [m]:");
-        double height = sc.nextDouble();
+        System.out.println("Podaj wzrost [m]:");//wyswietlenie uzytkownikowi komunikatu jaka wartosc ma wprowadzic z klawiatury
+        double height = sc.nextDouble(); //pobranie zmiennej z klawiatury
 
-        double bmi = weight/(height*height);
-        System.out.format("Twoje BMI to: %f", bmi);
+        double bmi = weight/(height*height); // wyliczenie bmi ze wzoru
+        System.out.format("Twoje BMI to: %f", bmi);//wyswietlenie uzytkownikowi komunikatu jaka jest wartosc jego bmi
 
 
 
 */
+//1.5+ ostatnia część + klasa BodyMassIndex
+/*
+        double w = 0, h = 0; //inicjalizacja zmiennych pomocniczych sluzacych do walidacji danych wejsciowych oraz, po poprawnym wprowadzeniu,  przekazywancyh do konstruktora obiektu klasy BodyMassIndex
+        Scanner in = new Scanner(System.in); //utworzenie obiektu typu Scanner, ktory przyjmuje jako parametr pole "in" statycznej klasy "System"- stanowi standardowy strumien wejscia, klawiature
+        System.out.println("Podaj wage [kg]: "); //wyswietlenie uzytkownikowi komunikatu jaka wartosc ma wprowadzic z klawiatury [wage w kg] i przejscie do nowej linii
+        while (w == 0) { //warunek walidacyjny, jesli prawda, to wystapil blad danych wejsciowych - powtorz petle
+            if(in.hasNextDouble()){ //warunek logiczny sprawdzajacy czy podana z klawiatury zostala wartosc typu double
+                w = in.nextDouble(); //przypisanie do zmiennej pomocniczej oznaczajacej wage wartosci pobranej z klawiatury
+                if(w <= 0){ //jesli podana liczba jest mniejsza lub rowna 0 - blad. waga czlowieka nie mzoe przyjac takiej wartosci
+                    System.err.println("Waga nie może być ujemna lub rowna 0"); //wyswietlenie na standardowe wyjscie (konsole) komuniaktu bledu przekazanego w nawiasie - tekst bedzie kolorem czerwonym
+                    in.nextLine(); //ponowne wprowadzenie wartosci z klawiatury
+                    w = 0; //przypisanie do zmiennej w wartosci 0, co powoduje spelnienie warunku logicznego w petli while i ponowne wykonanie sie petli oraz sprawdzenie czy wprowadzona wartosc jest poprawna
+                }
+            }else { //nie zostala wprowadzona liczba
+                System.err.println("Podaj liczbę");  //wyswietlenie na standardowe wyjscie (konsole) komuniaktu bledu przekazanego w nawiasie - tekst bedzie kolorem czerwonym (nie zostala podana wartosc typu double)
+                in.nextLine(); //ponowne wprowadzenie wartosci z klawiatury
+            }
+        }
 
+        System.out.println("Podaj wzrost [m]: "); //wyswietlenie uzytkownikowi komunikatu jaka wartosc ma wprowadzic z klawiatury [wzrost w m] i przejscie do nowej linii
+        while (h == 0) { //warunek walidacyjny, jesli prawda, to wystapil blad danych wejsciowych - powtorz petle
+            if(in.hasNextDouble()) { //warunek logiczny sprawdzajacy czy podana z klawiatury zostala wartosc typu double
+                h = in.nextDouble();  //przypisanie do zmiennej pomocniczej oznaczajacej wzrost wartosci pobranej z klawiatury
+                if(h <= 0) { //jesli podana liczba jest mniejsza lub rowna 0 - blad. wzrost czlowieka nie mzoe przyjac takiej wartosci
+                    System.err.println("Wzrost nie może być ujemny lub rowny 0"); //wyswietlenie na standardowe wyjscie (konsole) komuniaktu bledu przekazanego w nawiasie - tekst bedzie kolorem czerwonym
+                    in.nextLine(); //ponowne wprowadzenie wartosci z klawiatury
+                    h = 0; //przypisanie do zmiennej h wartosci 0, co powoduje spelnienie warunku logicznego w petli while i ponowne wykonanie sie petli oraz sprawdzenie czy wprowadzona wartosc jest poprawna
+                }
+            }else { //nie zostala wprowadzona liczba
+                System.err.println("Podaj liczbę"); //wyswietlenie na standardowe wyjscie (konsole) komuniaktu bledu przekazanego w nawiasie - tekst bedzie kolorem czerwonym (nie zostala podana wartosc typu double)
+                in.nextLine(); //ponowne wprowadzenie wartosci z klawiatury
+            }
+        }
+
+        in.close();  //zamkniecie standardowego wejscia
+
+        BodyMassIndex bmi = new BodyMassIndex(w, h); //utworzenie obiektu klasy BMI oraz przekazanie mu poprawnych parametrow odczytanych od uzytkownika (waga, wzrost)
+        System.out.println(bmi.getClassification()); //wyswietlenie rezultatu dzialanie metody pokazujacej info nt. indeksu BMI. wywoalana metoda  zwraca wartosc typu String, wiec trzeba ja podac na wyjscie metoda np. "println"
+
+//*/
     }
 }
